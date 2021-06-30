dcfa13be0946387e4ce9b5aae67f79ee5545f4c3
rv2212 <vreena2019@gmail.com>
2021-06-30 08:17:57 +0100

    Initial commit

---
+++ scraper/weather_scraper.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+class Scraper:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def getContentFromPage(url):
+        result = {}
+        soup = BeautifulSoup(requests.get(url).content, 'html.parser')
+
+        result['Temperature'] = soup.find('span', attrs={"class": "wr-value--temperature--c"}).text
+
+        # Wind speed
+        result['Wind_speed'] = soup.find('span', attrs={'class': "wr-value--windspeed wr-value--windspeed--mph"}).text
+
+        # time slot
+        result['time'] = soup.find('span', attrs={'class': 'wr-time-slot-primary__time'}).text
+
+        # Precipitation
+        result['Precipitation'] = soup.find('span', attrs={'class': "wr-u-font-weight-500"}).text
+
+        return result
+
+
+data = Scraper()
+print(data.getContentFromPage("https://www.bbc.co.uk/weather/2644605"))
