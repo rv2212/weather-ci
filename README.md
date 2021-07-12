b108a7093a21b553e267148d44985927a15da627
rv2212 <vreena2019@gmail.com>
2021-07-12 11:24:52 +0100

    few more updates

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # weather-ci
+
+Weather Scraper script extracts the current hour temp data from bbc weather. Jenkins (EC2)  build is triggered withe very git push.  
+
+The data is uploaded to S3 bucket.
+
+#### AWS resources :
+
+Cloudformation template is used to create EC2 and S3 bucket. 
+
--- scraper/weather_scraper.py
+++ scraper/weather_scraper.py
@@ -1,6 +1,7 @@
+import logging
 from bs4 import BeautifulSoup
 from datetime import datetime, timezone, date
-import pandas as pd
+from botocore.exceptions import ClientError
 import requests
 import boto3
 import json
@@ -42,19 +43,22 @@ class Scraper:
             print(f"Attribute not found {e}")
 
         else:
-            return result
+            with open("data.json", "w") as fp:
+                json.dump(result, fp)
+
+            return write_to_s3("ec2-s3-weather-v7s30dq4fxw", fp)
 
 
 def lambda_handler(event, context):
-    data = Scraper()
-    result = data.getContentFromPage()
+    weather_data = Scraper()
+    weather_data.getContentFromPage()
     bucketName = "hourly-weather"
     today = datetime.now(timezone.utc)
 
     fileName = "temp" + str(today) + ".json"
 
     s3 = boto3.client("s3")
-    uploadByteStream = bytes(json.dumps(result).encode('UTF-8'))
+    uploadByteStream = bytes(json.dumps(weather_data).encode('UTF-8'))
     s3.put_object(bucketName, fileName, uploadByteStream)
 
     print('Put Complete')
@@ -63,6 +67,17 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
+
+
+def write_to_s3(file_name, bucket_name):
+    s3 = boto3.client("s3")
+    PREFIX = 'weather-data/'
+    try:
+        res = s3.put_object(Body=file_name, bucket_name=bucket_name, key=PREFIX + '_DATA').put(Body=file_name)
+    except ClientError as e:
+        logging.error(e)
+    else:
+        return res
 
 
 data = Scraper()
