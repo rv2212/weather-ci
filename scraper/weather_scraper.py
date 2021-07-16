c02f146226ce366f1b2f09363925fe71a1632d6e
rv2212 <vreena2019@gmail.com>
2021-07-16 13:37:06 +0100

    updated the scraper script

--- scraper/weather_scraper.py
+++ scraper/weather_scraper.py
@@ -1,6 +1,7 @@
 import logging
 from bs4 import BeautifulSoup
 from datetime import datetime, timezone, date
+from botocore.exceptions import ClientError
 import requests
 import boto3
 import json
@@ -13,9 +14,9 @@ class Scraper:
 
     def getContentFromPage(self):
         """
-        Get the wether data from the page
+        Get the weather data from the page
         :param url: weather url
-        :return: weather data dictionary
+        :return: File to s3 bucket
         """
         try:
 
@@ -38,10 +39,30 @@ class Scraper:
             # Precipitation
             result['Precipitation'] = soup.find('span', attrs={'class': "wr-u-font-weight-500"}).text
 
-            return put_s3_object("ec2-s3-weather-v7s30dq4fxw", result)
+            return write_to_s3("ec2-s3-weather-v7s30dq4fxw", result)
 
         except AttributeError as e:
             logging.error(f"Attribute not found {e}")
+
+
+def write_to_s3(bucket_name, object_body):
+    """
+        Write a file to S3 bucket
+        :param bucket_name: Bucket to write
+        :param object_body: data to write
+        :return: True if file was uploaded else False
+        """
+    PREFIX = "weather-data" + "temp_" + datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p") + ".json"
+    try:
+        s3 = boto3.client('s3')
+        s3.put_object(Bucket=bucket_name, Body=bytes(json.dumps(object_body).encode("UTF-8")), Key=PREFIX,
+                      ACL='public-read',
+                      ContentType='application/json')
+        print("S3 file put done")
+    except ClientError as e:
+        logging.error('Error occurred :' + str(e))
+        return False
+    return True
 
 
 def lambda_handler(event, context):
@@ -64,17 +85,5 @@ def lambda_handler(event, context):
     }
 
 
-def put_s3_object(bucket_name, object_body):
-    PREFIX = "weather-data" + "/" + "temp_" + datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p") + ".json"
-    try:
-        s3 = boto3.client('s3')
-        s3.put_object(Bucket=bucket_name, Body=bytes(json.dumps(object_body).encode("UTF-8")), Key=PREFIX, ACL='public-read',
-                      ContentType='application/json')
-        print("S3 file put done")
-    except Exception as e:
-        logging.error('Error occurred :' + str(e))
-    return ""
-
-
 data = Scraper()
-print(data.getContentFromPage())
+data.getContentFromPage()
